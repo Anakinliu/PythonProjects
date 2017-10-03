0ba061c29a74c85522e2a277499bab2cacee12d7
ture_true <gugeliuyinquan@gmail.com>
2017-10-03 14:42:07 +0800

    add comment for binary_search

--- book/binary_search.py
+++ book/binary_search.py
@@ -1,16 +1,27 @@
+# coding: utf-8
+# 二分搜索
+
+
 def main_fun(source, target):
+    """
+    source -> 有序列表
+    target -> 查找目标
+
+    找到目标就返回列表索引, 否则返回None
+
+    """
     low = 0
     high = len(source) - 1
 
     while low <= high:
-        mid = (low + high) // 2
+        mid = (low + high) // 2  # python3地板除取整
         if target == source[mid]:
             return mid
         if target < source[mid]:
             high = mid - 1
         else:
             low = mid + 1
-    return -1
+    return None
 
 
 test_list = [1, 2, 4, 6, 22, 34, 55, 99, 192, 435, 999]
