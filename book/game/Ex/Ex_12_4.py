1a29f10b22976b79e5afaef8b525c1a6c5477953
ture_true <gugeliuyinquan@gmail.com>
2017-12-23 20:44:23 +0800

    finish ex

--- book/game/Ex/Ex_12_4.py
+++ book/game/Ex/Ex_12_4.py
@@ -10,7 +10,7 @@ import sys
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((1920, 1080))
     pygame.display.set_caption("Ex_12")
 
     # 主循环
@@ -19,7 +19,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            if event.type == pygame.KEY
+            if event.type == pygame.KEYDOWN:
+                """打印的是键对应的ASCII值"""
+                print(event.key)
 
         pygame.display.flip()
 
