70ff0fda95f410605a490343392aaa56d0a8ccca
anakinliu <gugeliuyinquan@gmail.com>
2020-03-13 23:15:42 +0800

    CodeWars 2020/3/13 Python 杨辉三角斜线求和结果老是差一点

--- CodeWars/6kyu/Easy Diagonal.py
+++ CodeWars/6kyu/Easy Diagonal.py
@@ -1,26 +1,27 @@
+import math
+
+def jc(x):
+    result = 1
+    for i in range(1, x+1):
+        result *= i
+    return result
+
+
+def c(n, m):
+    # return int(math.factorial(n)) / int((math.factorial(m)) * int(math.factorial(n - m)))
+    return jc(n) / (jc(m) * jc(n - m))
+
+
 def diagonal(n, p):
-    # your code
-    n = n + 1
-    matrix = []
-    result = 0
-    for i in range(n):
-        row = [0 for _ in range(i+1)]
-        row[0] = 1
-        for j in range(1, i):
-            row[j] = matrix[i-1][j] + matrix[i-1][j-1]
-            if j == p:
-                result += row[j]
-                break
-            pass
-        row[-1] = 1
-
-    return
+    return int(c(n, p) + c(n, p+1))
 
 
-def diagonal2(n, p):
+
+
     pass
 
 # 129100 5
-print(diagonal(20000, 40))
+print(diagonal(3591, 153))
+# print(c(66666666, 4))
 
 
