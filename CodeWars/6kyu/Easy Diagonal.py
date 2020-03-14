b9631328696374ccd78435f423ef8cdf763f93f4
anakinliu <gugeliuyinquan@gmail.com>
2020-03-14 09:39:12 +0800

    CodeWars 2020/3/14 Python 杨辉三角斜线错误原因操作符使用不当

--- CodeWars/6kyu/Easy Diagonal.py
+++ CodeWars/6kyu/Easy Diagonal.py
@@ -1,27 +1,20 @@
 import math
 
-def jc(x):
-    result = 1
-    for i in range(1, x+1):
-        result *= i
-    return result
-
-
 def c(n, m):
-    # return int(math.factorial(n)) / int((math.factorial(m)) * int(math.factorial(n - m)))
-    return jc(n) / (jc(m) * jc(n - m))
+    # 都是可以整除的超大整数，使用整数除法：//
+    return math.factorial(n) // (math.factorial(m) * math.factorial(n - m))
 
 
 def diagonal(n, p):
-    return int(c(n, p) + c(n, p+1))
-
-
-
-
+    return c(n, p) + c(n, p+1)
     pass
 
+from scipy.special import comb
+def diagonal(n, p):
+    return comb(n+1, p+1, exact=True)
+
 # 129100 5
-print(diagonal(3591, 153))
+print(diagonal(2155 , 51))
 # print(c(66666666, 4))
 
 
